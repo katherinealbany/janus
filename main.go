05c5fbe06bfe8176a4e7ef33d53dbbd6c4624ed2
Katherine Albany <git@kalbany.net>
2016-02-14 22:16:28 +0000

    Checkpoint

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "log"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os/exec"
+)
 
 func main() {
-	log.Println("Hello, World")
+	cmd := exec.Command("docker", "version")
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	scanner := bufio.NewScanner(stdout)
+	go func() {
+		for scanner.Scan() {
+			fmt.Printf("%s\n", scanner.Text())
+		}
+	}()
+
+	err = cmd.Start()
+	log.Println("Running...")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Waiting...")
+	err = cmd.Wait()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Finished with error: %v", err)
 }
